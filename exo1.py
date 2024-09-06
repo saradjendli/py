fc22b2a4e16083bef219996c0c0a8b8675698d47
saradjendli <sara.djendlii@gmail.com>
2024-09-06 11:03:18 +0200

    Add files via upload

---
+++ exo1.py
@@ -0,0 +1,12 @@
+moyenne=float(input("quelle est votre moyenne :  "))
+if moyenne>= 18:
+    print("Exellent")
+elif 14<=moyenne<18:
+    print("Tres bien")
+elif 10<=moyenne<14:
+    print("Assez bien")
+elif 5<=moyenne<10:
+    print("insuffisant")
+else :
+    print ("catastophique")
+    
